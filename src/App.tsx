9428cc91751a11ff47e08f0218060642bea8aada
nianoute <enzo.angot@gmail.com>
2023-10-13 12:25:58 +0200

    test: test socket io

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState, useEffect } from 'react';
+
+// import { ConnectionState } from './app/components/ConnectionState';
+import { ConnectionManager } from './app/components/ConnectionManager';
+import { socket } from './config/services/socket.service'
+import Event from './app/components/Event';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isConnected, setIsConnected] = useState(socket.connected);
+
+  useEffect(() => {
+    function onConnect() {
+      setIsConnected(true);
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+    }
+
+    socket.on('connect', onConnect);
+    socket.on('disconnect', onDisconnect);
+
+    return () => {
+      socket.off('connect', onConnect);
+      socket.off('disconnect', onDisconnect);
+    };
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>State: {'' + isConnected}</p>
+      <ConnectionManager />
+      <Event />
     </>
   )
 }
---
+++ src/app/components/Event.tsx
@@ -0,0 +1,23 @@
+import { socket } from "../../config/services/socket.service";
+import { useEffect } from "react";
+
+const Event = () => {
+
+    useEffect(() => {
+        socket.on('connect', function () {
+
+            socket.emit('events', { test: 'test' });
+            socket.emit('identity', 0, response =>
+                console.log('Identity:', response),
+            );
+        });
+    }, []);
+
+
+    return (
+        <div>
+        </div>
+    );
+};
+
+export default Event;
